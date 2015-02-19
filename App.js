9dba70807d834f0e239c61830500f1df5be45dd0
Nick Musaelian <nmusaelian@nmusaellian-mbr.f4tech.com>
2015-02-19 12:37:15 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,89 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.TimeboxScopedApp',
+    componentCls: 'app',
+    scopeType: 'iteration',
+    comboboxConfig: {
+        fieldLabel: 'Select iteration:',
+        labelWidth: 100,
+        width: 300
+    },
+    onScopeChange: function() {
+        Ext.create('Rally.data.wsapi.Store',{
+	    model: 'User Story',
+	    fetch: ['FormattedID','Name','PlanEstimate'],  
+	    limit: Infinity,
+	    autoLoad: true,
+	    filters: [this.getContext().getTimeboxScope().getQueryFilter()],
+	    sorters: [
+		{
+		property: 'DragAndDropRank',
+		direction: 'DESC'
+		}
+	    ],
+	    listeners: {
+		load: this._onStoriesLoaded,
+		scope: this
+	    }
+	    });
+    },
+    _onStoriesLoaded: function(store,records){
+        var myData = [];
+        //in case we want to track them separately
+        //var estimatedStories = []; 
+        //var unestimatedStories = [];
+	console.log(records.length);
+        var estimated = false;
+        _.each(records, function(record){
+            //if (record.get('PlanEstimate')) {
+            //    estimatedStories.push(record);
+            //}
+            //else{
+            //    unestimatedStories.push(record);
+            //}
+            estimated = record.get('PlanEstimate') ? true : false
+            console.log(estimated);
+            var obj = {
+                isEstimated: estimated,
+                Name: record.get('Name'),
+                FormattedID: record.get('FormattedID'),
+                PlanEstimate: record.get('PlanEstimate')
+            };
+            myData.push(obj);
+        },this);
+        //console.log(estimatedStories.length, unestimatedStories.length);
+        this._makeGrid(myData);
+    },
+    _makeGrid:function(myData){
+        if(this.down('#storyGrid')){
+            this.down('#storyGrid').destroy();
+        }
+        var gridStore = Ext.create('Rally.data.custom.Store', {
+	  data: myData,
+	  limit:Infinity,
+          groupField: 'isEstimated'
+        });
+        this.add({
+            xtype: 'rallygrid',
+            itemId: 'storyGrid',
+            store: gridStore,
+            features: [
+                       {
+                        ftype:'grouping',
+                        groupHeaderTpl: 'Estimated: {name} ({rows.length} {[values.rows.length > 1 ? "Stories" : "Story"]})'
+                       }
+                       ],
+            columnCfgs:[
+                {
+                    text: 'ID', dataIndex: 'FormattedID'
+                },
+                {
+                  text: 'Name', dataIndex: 'Name'
+                },
+                {
+                  text: 'PlanEstimate', dataIndex: 'PlanEstimate'
+                }
+            ]
+        });
+    }
+    
+});
